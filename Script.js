1740f8bd5b69ce14172a96a173b0e7bb5509f0bb
Hanief Bintang Tri Buana <haniefbintang3@gmail.com>
2024-12-30 22:26:59 +0700

    Update Script.js
    fitur toggle theme

--- Script.js
+++ Script.js
@@ -9,8 +9,26 @@ function clearResult() {
 function calculateResult() {
   const resultField = document.getElementById('result');
   try {
-      resultField.value = eval(resultField.value);
+    resultField.value = eval(resultField.value);
   } catch (error) {
-      resultField.value = 'Error';
+    resultField.value = 'Error';
+  }
+}
+
+function toggleTheme() {
+  const body = document.body;
+  const video = document.getElementById('background-video');
+  const toggleButton = document.getElementById('theme-toggle');
+
+  if (body.classList.contains('kurumi-theme')) {
+    body.classList.remove('kurumi-theme');
+    body.classList.add('arona-theme');
+    video.src = "https://i.imgur.com/GumIkSy.mp4";
+    toggleButton.style.backgroundImage = "url('https://i.imgur.com/wedWg0h.png')";
+  } else {
+    body.classList.remove('arona-theme');
+    body.classList.add('kurumi-theme');
+    video.src = "https://i.imgur.com/btbvvwh.mp4";
+    toggleButton.style.backgroundImage = "url('https://i.imgur.com/uAdeaYN.png')";
   }
 }
